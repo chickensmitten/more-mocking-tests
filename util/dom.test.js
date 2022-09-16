7a8065e51e6850261b9f2a03a263013f0f48a361
sheng yeong <chia.shengyeong@gmail.com>
2022-09-16 11:40:58 +0800

    resetting virtual dom

--- util/dom.test.js
+++ util/dom.test.js
@@ -1,6 +1,6 @@
 import fs from "fs";
 import path from "path";
-import { expect, it, vi } from "vitest";
+import { beforeAll, beforeEach, expect, it, vi } from "vitest";
 import { Window } from "happy-dom";
 import { showError } from "./dom";
 
@@ -9,9 +9,14 @@ const htmlDocumentContent = fs.readFileSync(htmlDocPath).toString();
 
 const window = new Window();
 const document = window.document;
-document.write(htmlDocumentContent);
 vi.stubGlobal("document", document);
 // setting up frontend virtual html page in the test file with the code above.
+
+beforeEach(() => {
+  // have to reset the virtual document because it carries over to the other tests
+  document.body.innerHTML = "";
+  document.write(htmlDocumentContent);
+});
 
 it("should add an error paragraph to the id='errors' element", () => {
   showError("Test");
@@ -22,3 +27,9 @@ it("should add an error paragraph to the id='errors' element", () => {
   expect(errorParagraph).not.toBeNull();
 })
 
+it("should not contain an error paragraph initially", () => {
+  const errorsEl = document.getElementById("errors");
+  const errorParagraph = errorsEl.firstElementChild;
+
+  expect(errorParagraph).toBeNull();  
+})
